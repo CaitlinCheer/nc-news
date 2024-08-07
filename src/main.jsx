2c0b80117e698a839d3d14083e86f1ed293a36df
Caitlin <caitlincheer043@gmail.com>
2024-08-07 16:23:42 +0100

    Added delete comment function to user

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { UsersProvider } from "./Context/Users.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root")).render(
+  <UsersProvider>
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>
+  </UsersProvider>
 );
